caf395ecba019a6c3b13436f7de32424bcc53844
hung1306 <mycode13062002@gmail.com>
2024-04-22 13:07:07 +0700

    Feat: add slider for homepage

---
+++ front-end/src/components/SlideShow/SlideShow.jsx
@@ -0,0 +1,62 @@
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import slider1 from '../../assets/slider/slider1.jpg';
+import slider2 from '../../assets/slider/slider2.jpg';
+import slider3 from '../../assets/slider/slider3.jpg';
+
+const imgs = [
+  slider1,
+  slider2,
+  slider3
+]
+
+const SlideShow = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+
+  return (
+    <>
+      <style>
+        {`
+          .slick-next:before, .slick-prev:before {
+            font-size: 30px;
+            color: #07bc0c;
+          }
+
+          .slick-next, .slick-prev {
+            height: 50px;
+            width: 50px;
+            position: absolute;
+            top: 50%;
+            transform: translateY(-50%);
+            border-radius: 50%;
+            z-index: 1;
+          }
+
+          .slick-next {
+            right: 10px;
+          }
+
+          .slick-prev {
+            left: 10px;
+          }
+        `}
+      </style>
+      <Slider {...settings} className="w-1/2 h-96 bg-fourth">
+        {imgs.map((image, index) => (
+          <div key={index} className="h-96  rounded-lg">
+            <img src={image} alt={`Slide ${index}`} className="w-full h-full" />
+          </div>
+        ))}
+      </Slider>
+    </>
+  );
+};
+
+export default SlideShow;
--- front-end/src/pages/Customer/HomePage/HomePage.jsx
+++ front-end/src/pages/Customer/HomePage/HomePage.jsx
@@ -1,11 +1,31 @@
-import HeaderCustomer from "../../../components/HeaderCustomer/HeaderCustomer"
+import HeaderCustomer from "../../../components/HeaderCustomer/HeaderCustomer";
+import SlideShow from "../../../components/SlideShow/SlideShow";
 
 function HomePage() {
     return (
-        <div>
-            <HeaderCustomer />
+        <div className="h-screen flex flex-col">
+        <HeaderCustomer />
+        <div className="flex justify-center my-10" >
+            {" "}
+            {/* Add flex here */}
+            <SlideShow className="w-2/3" />
+            <div className="w-1/4 flex flex-col items-center justify-center h-full">
+                <img
+                    src="https://firebasestorage.googleapis.com/v0/b/storgeimage.appspot.com/o/SliderImage%2Fslider4.jpg?alt=media&token=135edbe7-ab5b-4145-af9e-2565f768120c"
+                    alt="Slider Image"
+                    className="w-3/4 h-1/2 object-cover rounded-t-lg"
+                />
+                <div className="bg-primary w-3/4 h-1/2 rounded-b-lg">
+                    <p className="mt-9 text-secondary text-3xl text-center ">
+                        Tìm hiểu thêm về 
+                    </p>
+                    <p className="text-5xl ml-5 font-bold text-center text-secondary" > Agrimart</p>
+                </div>
+                
+            </div>
         </div>
-    )
+        </div>
+    );
 }
 
-export default HomePage
+export default HomePage;
