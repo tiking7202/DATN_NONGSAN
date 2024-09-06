71c76a526a7ba05b86950e9f593fbd47bd718eae
hung1306 <mycode13062002@gmail.com>
2024-09-06 08:28:21 +0700

    wip

--- back-end/controllers/categoryController.js
+++ back-end/controllers/categoryController.js
@@ -21,37 +21,3 @@ exports.getCategoryById = async (req, res) => {
         res.status(500).json({ message: 'Internal Server Error' });
     }
 };
-
-exports.createCategory = async (req, res) => {
-    const { name, description } = req.body;
-    try {
-        const newCategory = await pool.query('INSERT INTO categories (name, description) VALUES ($1, $2) RETURNING *', [name, description]);
-        res.status(201).json(newCategory.rows[0]);
-    } catch (error) {
-        console.error('Error creating category:', error);
-        res.status(500).json({ message: 'Internal Server Error' });
-    }
-};
-
-exports.updateCategory = async (req, res) => {
-    const { id } = req.params;
-    const { name, description } = req.body;
-    try {
-        const updatedCategory = await pool.query('UPDATE categories SET name = $1, description = $2 WHERE id = $3 RETURNING *', [name, description, id]);
-        res.json(updatedCategory.rows[0]);
-    } catch (error) {
-        console.error('Error updating category:', error);
-        res.status(500).json({ message: 'Internal Server Error' });
-    }
-};
-
-exports.deleteCategory = async (req, res) => {
-    const { id } = req.params;
-    try {
-        await pool.query('DELETE FROM categories WHERE id = $1', [id]);
-        res.json({ message: 'Category deleted successfully' });
-    } catch (error) {
-        console.error('Error deleting category:', error);
-        res.status(500).json({ message: 'Internal Server Error' });
-    }
-};
--- back-end/routes/category.js
+++ back-end/routes/category.js
@@ -4,8 +4,6 @@ const categoryController = require('../controllers/categoryController');
 
 router.get('/category', categoryController.getCategories);
 router.get('/category/:id', categoryController.getCategoryById);
-router.post('/category', categoryController.createCategory);
-router.put('/category/:id', categoryController.updateCategory);
-router.delete('/category/:id', categoryController.deleteCategory);
+
 
 module.exports = router;
